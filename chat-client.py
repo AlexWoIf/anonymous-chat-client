5b5b3d359d8dddc5c0334b36b2e80ff3d17761ef
AlexWoIf <alexey.barannikov@gmail.com>
2024-06-16 13:01:28 +0300

    test message output

--- chat-client.py
+++ chat-client.py
@@ -1,10 +1,14 @@
 import asyncio
 import gui
 
-loop = asyncio.get_event_loop()
 
-messages_queue = asyncio.Queue()
-sending_queue = asyncio.Queue()
-status_updates_queue = asyncio.Queue()
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
 
-loop.run_until_complete(gui.draw(messages_queue, sending_queue, status_updates_queue))
+    messages_queue = asyncio.Queue()
+    sending_queue = asyncio.Queue()
+    status_updates_queue = asyncio.Queue()
+
+    messages_queue.put_nowait('Привет обитателям чата!')
+    messages_queue.put_nowait('Как дела?')
+    loop.run_until_complete(gui.draw(messages_queue, sending_queue, status_updates_queue))
