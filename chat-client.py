1ca74f441b6f818b8a454486da314434e64bc75d
AlexWoIf <alexey.barannikov@gmail.com>
2024-06-17 09:51:27 +0300

    save all messages to chat and restore them on startup

--- chat-client.py
+++ chat-client.py
@@ -1,3 +1,4 @@
+import aiofiles
 import asyncio
 import logging
 import socket
@@ -13,26 +14,37 @@ def import_config():
     parser.add('--host', '--HOST', help='server address')
     parser.add('--reading_port', '--READING_PORT', help='server port')
     parser.add('--logfile', '--LOGFILE', help='log filepath')
-
     args, _ = parser.parse_known_args()
     print(args)
-
     return vars(args)
+
+
+async def write_to_file(file_path, content):
+    async with aiofiles.open(file_path, mode='a', ) as f:
+        await f.write(content.strip())
+
+
+async def read_file(filepath, messages_queue):
+    async with aiofiles.open(filepath, mode='rb') as f:
+        async for line in f:
+            messages_queue.put_nowait(line.decode('cp1251').strip())
+
 
 async def read_message(config):
     host, port, _ = config.values()
     reader, _ = await asyncio.open_connection(host, port)
     received_msg = await reader.readline()
-    decoded_msg = received_msg.decode().strip()
-    logging.debug(decoded_msg)
+    decoded_msg = received_msg.decode()
+    # logging.debug(decoded_msg)
     return decoded_msg
 
 
 async def print_message(messages_queue, config, ):
     while True:
         message = await read_message(config)
-        messages_queue.put_nowait(message)
-        # await asyncio.sleep(delay)
+        messages_queue.put_nowait(message.strip())
+        filepath = config.get('logfile', f'{__name__}.log')
+        await write_to_file(filepath, message)
 
 
 async def main():
@@ -41,6 +53,8 @@ async def main():
     status_updates_queue = asyncio.Queue()
 
     config = import_config()
+
+    await read_file(config['logfile'], messages_queue)
 
     await asyncio.gather(
         gui.draw(messages_queue, sending_queue, status_updates_queue),
