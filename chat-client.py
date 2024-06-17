4d19f35c550f8a4ba8b64bdc75c5c63e5c74aecc
AlexWoIf <alexey.barannikov@gmail.com>
2024-06-17 23:15:52 +0300

    add authorisation method

--- chat-client.py
+++ chat-client.py
@@ -1,22 +1,66 @@
-import aiofiles
 import asyncio
+import json
 import logging
 import socket
 import time
+from contextlib import asynccontextmanager
 
+import aiofiles
 import configargparse
 
 import gui
+
+HELLO_PROMPT = 'Hello %username%! Enter your personal hash or leave it empty to create new account.'
+WELCOME_PROMPT = 'Welcome to chat! Post your message below. End it with an empty line.'
+REGISTER_PROMPT = 'Enter preferred nickname below:'
 
 
 def import_config():
     parser = configargparse.ArgParser(default_config_files=['.env', ])
     parser.add('--host', '--HOST', help='server address')
     parser.add('--reading_port', '--READING_PORT', help='server port')
+    parser.add('--writing_port', '--WRITING_PORT', help='server port')
     parser.add('--logfile', '--LOGFILE', help='log filepath')
+    parser.add('--token', '--TOKEN', help='user token')
+    parser.add('--nickname')
+    # parser.add('-m', required=True, help='message text')
+
     args, _ = parser.parse_known_args()
     print(args)
     return vars(args)
+
+
+@asynccontextmanager
+async def connection_manager(host, port):
+    reader, writer = await asyncio.open_connection(host, port)
+    try:
+        yield reader, writer
+    finally:
+        writer.close()
+        await writer.wait_closed()
+
+
+async def authorise(config):
+    host = config['host']
+    port = config['writing_port']
+    token = config['token']
+    async with connection_manager(host, port) as (reader, writer):
+        received_msg = await reader.readline()
+        decoded_msg = received_msg.decode().strip()
+        logging.debug(decoded_msg)
+
+        if decoded_msg != HELLO_PROMPT:
+            raise RuntimeError
+
+        writer.write(f'{token}\n'.encode())
+        await writer.drain()
+        answer = await reader.readline()
+        decoded_answer = answer.decode().strip()
+        logging.debug(decoded_answer)
+        if decoded_answer == 'null':
+            raise ValueError
+        nickname = json.loads(decoded_answer).get('nickname')
+    return nickname
 
 
 async def write_message_to_file(file_path, content):
@@ -68,6 +112,9 @@ async def main():
     filepath = config.get('logfile', f'{__name__}.log')
     config['logfile'] = filepath
     await read_messages_from_file(filepath, messages_queue)
+
+    if config.get('token') is not None:
+        nickname = await authorise(config)
 
     await asyncio.gather(
         gui.draw(messages_queue, sending_queue, status_updates_queue),
