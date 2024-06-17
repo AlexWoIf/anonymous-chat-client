6ee1df4156918128fa27283c70802bbac05bd27a
AlexWoIf <alexey.barannikov@gmail.com>
2024-06-17 19:27:13 +0300

    get message text from gui

--- chat-client.py
+++ chat-client.py
@@ -19,19 +19,20 @@ def import_config():
     return vars(args)
 
 
-async def write_to_file(file_path, content):
+async def write_message_to_file(file_path, content):
     async with aiofiles.open(file_path, mode='a', ) as f:
-        await f.write(content.strip())
+        await f.write(content)
 
 
-async def read_file(filepath, messages_queue):
+async def read_messages_from_file(filepath, messages_queue):
     async with aiofiles.open(filepath, mode='rb') as f:
         async for line in f:
             messages_queue.put_nowait(line.decode('cp1251').strip())
 
 
-async def read_message(config):
-    host, port, _ = config.values()
+async def read_message_from_chat(config):
+    host = config['host']
+    port = config['reading_port']
     reader, _ = await asyncio.open_connection(host, port)
     received_msg = await reader.readline()
     decoded_msg = received_msg.decode()
@@ -41,10 +42,16 @@ async def read_message(config):
 
 async def print_message(messages_queue, config, ):
     while True:
-        message = await read_message(config)
+        message = await read_message_from_chat(config)
         messages_queue.put_nowait(message.strip())
-        filepath = config.get('logfile', f'{__name__}.log')
-        await write_to_file(filepath, message)
+        filepath = config['logfile']
+        await write_message_to_file(filepath, message)
+
+
+async def read_message_from_gui(sending_queue):
+    while True:
+        msg = await sending_queue.get()
+        logging.debug(msg)
 
 
 async def main():
@@ -54,11 +61,18 @@ async def main():
 
     config = import_config()
 
-    await read_file(config['logfile'], messages_queue)
+    logging.basicConfig(
+        format='%(levelname)s:%(filename)s:[%(asctime)s] %(message)s',
+        level=logging.DEBUG,
+    )
+    filepath = config.get('logfile', f'{__name__}.log')
+    config['logfile'] = filepath
+    await read_messages_from_file(filepath, messages_queue)
 
     await asyncio.gather(
         gui.draw(messages_queue, sending_queue, status_updates_queue),
-        print_message(messages_queue, config))
+        print_message(messages_queue, config),
+        read_message_from_gui(sending_queue))
 
 
 if __name__ == '__main__':
